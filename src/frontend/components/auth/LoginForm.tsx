8213dadceec6c1dd8022212c9ac99bdffa48141e
hienviluong125 <hienviluong125@gmail.com>
2021-12-26 14:17:19 +0700

    feat: Style login, register with Material UI

--- src/frontend/components/auth/LoginForm.tsx
+++ src/frontend/components/auth/LoginForm.tsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react"
 import { loginService } from "@frontend/services/authService"
 import { useRouter } from 'next/router'
+import { Alert, Button, Grid, TextField } from "@mui/material"
 
 const LoginForm: React.FC = () => {
   const router = useRouter()
   const [email, setEmail] = useState<string>("")
   const [password, setPassword] = useState<string>("")
+  const [error, setError] = useState<string>("")
 
   const onHandleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-
     e.preventDefault()
     loginService({ email, password })
       .then(() => {
         router.push("/properties")
-      }).catch(err => {
-        console.log({err})
+      }).catch(() => {
+        setError("Email or password is invalid")
       })
   }
 
@@ -27,11 +28,51 @@ const LoginForm: React.FC = () => {
   }
 
   return (
-    <form onSubmit={onHandleSubmit}>
-      <input type="email" value={email} onChange={onHandleChangeEmail} />
-      <input type="password" value={password} onChange={onHandleChangePassword} />
-      <button>Submit</button>
-    </form>
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    >
+
+      <Grid item xs={3}>
+        {
+          error &&
+          <Alert
+            severity="error"
+            onClose={() => setError("")}
+          >
+            {error}
+          </Alert>
+        }
+
+        <form onSubmit={onHandleSubmit}>
+          <h1>Please sign in</h1>
+          <TextField
+            sx={{ my: 1 }}
+            label='Email'
+            placeholder='Enter email'
+            fullWidth
+            required
+            type="email"
+            value={email}
+            onChange={onHandleChangeEmail}
+          />
+          <TextField
+            sx={{ my: 1 }}
+            label='Password'
+            placeholder='Enter password'
+            fullWidth
+            required
+            type='password'
+            value={password}
+            onChange={onHandleChangePassword}
+          />
+          <Button sx={{ my: 1 }} type='submit' color='primary' variant="contained" fullWidth>Sign in</Button>
+        </form>
+      </Grid>
+    </Grid>
   )
 }
 
--- src/frontend/components/auth/RegisterForm.tsx
+++ src/frontend/components/auth/RegisterForm.tsx
@@ -1,6 +1,102 @@
+import React, { useState } from "react"
+import { registerService } from "@frontend/services/authService"
+import { useRouter } from 'next/router'
+import { Alert, Button, Grid, TextField } from "@mui/material"
+
 const RegisterForm = () => {
+  const router = useRouter()
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [passwordConfirmation, setPasswordConfirmation] = useState<string>("")
+  const [error, setError] = useState<string>("")
+
+  const onHandleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    if (password !== passwordConfirmation) {
+      setError("Password confirmation doesn't match password")
+      return
+    }
+
+    registerService({ email, password })
+      .then(() => {
+        router.push("/properties")
+      }).catch(err => {
+        if (err.response.data.message) {
+          setError(err.response.data.message)
+        } else {
+          setError("Failure to register an account")
+        }
+      })
+  }
+
+  const onHandleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setEmail(e.target.value)
+  }
+
+  const onHandleChangePassword = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setPassword(e.target.value)
+  }
+
+  const onHandleChangePasswordConfirmation = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setPasswordConfirmation(e.target.value)
+  }
+
   return (
-    <h1>Register page</h1>
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    >
+
+      <Grid item xs={3}>
+        {
+          error &&
+          <Alert
+            severity="error"
+            onClose={() => setError("")}
+          >
+            {error}
+          </Alert>
+        }
+
+        <form onSubmit={onHandleSubmit}>
+          <h1>Register an account</h1>
+          <TextField
+            sx={{ my: 1 }}
+            label='Email'
+            placeholder='Enter email'
+            fullWidth
+            required
+            type="email"
+            value={email}
+            onChange={onHandleChangeEmail}
+          />
+          <TextField
+            sx={{ my: 1 }}
+            label='Password'
+            placeholder='Enter password'
+            fullWidth
+            required
+            type='password'
+            value={password}
+            onChange={onHandleChangePassword}
+          />
+          <TextField
+            sx={{ my: 1 }}
+            label='Password confirmation'
+            placeholder='Enter password confirmation'
+            fullWidth
+            required
+            type='password'
+            value={passwordConfirmation}
+            onChange={onHandleChangePasswordConfirmation}
+          />
+          <Button sx={{ my: 1 }} type='submit' color='primary' variant="contained" fullWidth>Sign in</Button>
+        </form>
+      </Grid>
+    </Grid>
   )
 }
 
